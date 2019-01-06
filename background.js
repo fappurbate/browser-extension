5bcf74edda3485667f850642e194f99983a3c82c
Kothique Felixx <kothique@protonmail.ch>
2019-01-06 12:57:54 +0300

    Detect broadcaster username

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const state = window.kothique = {};
-window.kothique.backend = 'http://localhost:8887';
-window.kothique.activeTabId = null;
+state.backend = 'http://localhost:8887';
+state.activeTabId = null;
+state.broadcaster = null;
 
 chrome.runtime.onInstalled.addListener(function () {
 });
@@ -15,7 +16,15 @@ function refresh() {
 
 chrome.debugger.onEvent.addListener(function (source, method, params) {
   if (method === 'Network.webSocketFrameSent') {
-    const data = params.response.payloadData;
+    const payload = params.response.payloadData;
+
+    if (payload[0] === '[') {
+      const data = JSON.parse(JSON.parse(payload)[0]);
+
+      if (data.method === 'joinRoom') {
+        state.broadcaster = data.data.room;
+      }
+    }
   }
 
   else if (method === 'Network.webSocketFrameReceived') {
@@ -46,28 +55,31 @@ chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
   if (tabId === state.activeTabId) {
     console.log('Disconnected from the broadcast page.');
     state.activeTabId = null;
+    state.broadcaster = null;
   }
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.active) {
+  if (changeInfo.status === 'complete') {
     if (tab.url.indexOf('chaturbate.com/b/') === -1) { return; }
 
     console.log(`Chaturbate broadcast open at ${tab.url}`);
-
-    if (state.activeTabId !== null) {
-      console.log(`Detaching from the old broadcast page...`);
-      chrome.debugger.detach({ tabId: state.activeTabId });
-      state.activeTabId = null;
-    }
-
+    state.broadcaster = null;
     refresh();
 
-    console.log(`Attaching debugger...`);
-    chrome.debugger.attach({ tabId: tab.id }, '1.1', function () {
-      state.activeTabId = tab.id;
-      chrome.debugger.sendCommand({ tabId: tab.id }, 'Network.enable');
-    });
+    if (tabId !== state.activeTabId) {
+      if (state.activeTabId !== null) {
+        console.log(`Detaching debugger from the old broadcast page...`);
+        chrome.debugger.detach({ tabId: state.activeTabId });
+        state.activeTabId = null;
+      }
+
+      console.log(`Attaching debugger...`);
+      chrome.debugger.attach({ tabId }, '1.1', function () {
+        state.activeTabId = tabId;
+        chrome.debugger.sendCommand({ tabId }, 'Network.enable');
+      });
+    }
   }
 });
 
@@ -80,6 +92,7 @@ function onTip(from, amount) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
+      broadcaster: state.broadcaster,
       tipper: from,
       amount
     })
--- popup.js
+++ popup.js
@@ -6,8 +6,10 @@ const state = bg.kothique;
 // Status
 const status = document.getElementById('status');
 status.innerHTML = state.activeTabId === null
-  ? 'Disconnected'
-  : 'Connected';
+  ? `<span class="disconnected">Disconnected</span>`
+  : `<span class="connected">
+      Connected${state.broadcaster ? ` as ${state.broadcaster}` : ''}
+    </span>`;
 
 // Backend address setting
 const backendInput = document.getElementById('backend');
